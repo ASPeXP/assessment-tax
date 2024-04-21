719bf7eb37d7adf953b7ec86c90247f5c6a41962
aspexp <ipathipad@gmail.com>
2024-04-21 22:09:51 +0700

    completed implement deducted donation from totalincome

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,18 +11,17 @@ import (
 	"github.com/aspexp/assessment-tax/tax"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 type RequestBody struct {
-TotalIncome float64 `json:"totalIncome"`
+	TotalIncome float64 `json:"totalIncome"`
   	Wht  float64 `json:"wht"`
 	Allowances []Allowance `json:"allowances"`
 }
 type Allowance struct {
 	
-	AllowanceType string  `json:"allowanceType"`
-	Allowance_amount float64 `json:"allowance_amount"`
+	Type string  `json:"allowanceType"`
+	Amount float64 `json:"amount"`
  
   
 }
@@ -30,8 +30,8 @@ func main() {
 	
 	e := echo.New()
 	  // Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
@@ -47,9 +47,15 @@ func main() {
 			return c.String(http.StatusBadRequest, err.Error()) 
 		}
 
-		personalDeducted := 60000.00
-		result := tax.TaxHandler(body.TotalIncome, body.Wht, personalDeducted)
-
+		var pti = tax.PersonalTaxInfo{
+			Income: body.TotalIncome,
+			Wht: body.Wht,
+			PersonalDeducted: 60000.00,
+			Donation: body.Allowances[0].Amount,
+		}
+		result := tax.TaxHandler(pti)
+		fmt.Println(result)
+		fmt.Printf("donation %.2f", pti.Donation)
 		return c.JSON(http.StatusOK, result )
 	})
 
