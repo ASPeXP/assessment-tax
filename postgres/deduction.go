513f182229f03f5fe345900e1a7059fd5aa52e01
aspexp <ipathipad@gmail.com>
2024-04-25 21:48:54 +0700

    completed TestPOSTKReceiptDeduction functino

--- postgres/deduction.go
+++ postgres/deduction.go
@@ -28,6 +28,24 @@ func (p *Postgres) GetPersonalDeduction() (Deduction, error ){
 	
 	return dd, err 
 }
+func (p *Postgres) GetKReceiptDeduction() (Deduction, error ){
+	
+	rows, err := p.Db.Query("select * from personal_deduct where id=2")
+	// dd := Deduction{}
+	if err != nil {
+		return Deduction{}, err 
+	}
+	defer rows.Close()
+	var dd Deduction
+	for rows.Next() {
+		err = rows.Scan(&dd.ID, &dd.Type, &dd.Amount, &dd.Created_at)
+		if err != nil {
+			return dd, err 
+		}
+	}
+	
+	return dd, err 
+}
 func (p *Postgres) PostPersonalDeduction(amount float64) (Deduction, error){
 
 	created_time := time.Now()
@@ -44,6 +62,27 @@ func (p *Postgres) PostPersonalDeduction(amount float64) (Deduction, error){
 		dd.Amount = amount 
 		dd.Type = "personal"
 		dd.ID = 1
+		dd.Created_at = created_time
+	}
+	
+	return dd, nil 
+}
+func (p *Postgres) PostKReceiptDeduction(amount float64) (Deduction, error){
+
+	created_time := time.Now()
+	result, err := p.Db.Exec(`Insert into personal_deduct(id, type, amount, created_at) values(2, $1, $2, $3)`, "k-receipt", amount, created_time)
+	var dd Deduction
+	if err != nil {
+		return dd, err 
+	}
+	rowAffected, err  := result.RowsAffected()
+	if err != nil {
+		return dd, err 
+	}
+	if rowAffected > 0 {
+		dd.Amount = amount 
+		dd.Type = "k-receipt"
+		dd.ID = 2
 		dd.Created_at = created_time
 	}
 	
@@ -70,4 +109,26 @@ func (p *Postgres) UpdatePersonalDeduction(amount float64) (Deduction, error){
 	
 	return dd, nil 
 }
+func (p *Postgres) UpdateKReceiptDeduction(amount float64) (Deduction, error){
+
+	created_time := time.Now()
+	result, err := p.Db.Exec(`Update personal_deduct set amount=$1, created_at=$2 where id=2`, amount, created_time)
+	var dd Deduction
+	if err != nil {
+		return dd, err 
+	}
+	rowAffected, err  := result.RowsAffected()
+	if err != nil {
+		return dd, err 
+	}
+	if rowAffected > 0 {
+		dd.Amount = amount 
+		dd.Type = "k-receipt"
+		dd.ID = 2
+		dd.Created_at = created_time
+	}
+	
+	return dd, nil 
+}
+
 
--- postgres/deduction_test.go
+++ postgres/deduction_test.go
@@ -12,7 +12,7 @@ import (
 
 
 func TestPOSTPersonalDeduction(t *testing.T) {
-		amount := 10001.0
+		amount := 60000.0
 		userName := os.Getenv("ADMIN_USERNAME")
 		pwd := os.Getenv("ADMIN_PASSWORD")
 		host := os.Getenv("host")
@@ -47,6 +47,56 @@ func TestPOSTPersonalDeduction(t *testing.T) {
 		}
 	}else{
 		deduction, err = p.UpdatePersonalDeduction(amount)
+		if err != nil {
+			log.Fatal(err )
+		}
+	}
+		
+		got := deduction.Amount
+	
+	if exp != got {
+		t.Errorf("expect %f but got %f", exp, got)
+	}
+
+	
+}
+
+func TestPOSTKReceiptDeduction(t *testing.T) {
+		amount := 70000.0
+		userName := os.Getenv("ADMIN_USERNAME")
+		pwd := os.Getenv("ADMIN_PASSWORD")
+		host := os.Getenv("host")
+		db_port := os.Getenv("db_port")
+		db_name := os.Getenv("database_name")
+		
+		connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", userName, pwd, host, db_port, db_name)
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			// Handle the error
+			panic(err)
+		}
+		defer db.Close()
+		
+		err = db.Ping()
+		if err != nil {
+			log.Fatal(err )
+		}
+		p := &Postgres{Db: db}
+
+	exp := amount
+		
+	deduction, err := p.GetKReceiptDeduction()
+	if err != nil {
+		log.Fatal(err )
+	}
+	if deduction == (Deduction{}){
+		// return "no deduction available"
+		deduction, err = p.PostKReceiptDeduction(amount)
+		if err != nil {
+			log.Fatal(err )
+		}
+	}else{
+		deduction, err = p.UpdateKReceiptDeduction(amount)
 		if err != nil {
 			log.Fatal(err )
 		}
